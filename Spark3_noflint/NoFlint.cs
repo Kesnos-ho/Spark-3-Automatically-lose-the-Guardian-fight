be6fbdd6c18ecc779c45ae84184350afe3f2cc22
Kesnos-ho <76844828+Kesnos-ho@users.noreply.github.com>
2025-10-08 18:29:47 +0200

    Add files via upload

---
+++ Spark3_noflint/NoFlint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Spark3FlintInstantDeath
+{
+    public class FlintInstantDeath : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
+            if (sceneName == "[STAGE 02 - BOSS FLINT]"|| sceneName == "[STAGE 03 - BOSS FLINT SECOND]")
+            {
+                HurtControl hurtControl = GameObject.Find("Player_Fark").GetComponent<HurtControl>();
+                hurtControl.isDead=true;
+            }
+            }
+
+    }
+}
